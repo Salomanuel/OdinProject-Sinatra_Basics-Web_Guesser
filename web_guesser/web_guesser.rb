e564a42985cce8b0f90b8c43bee730bbde658355
Salomanuel <salomanuel.aux+code@gmail.com>
2017-02-09 17:27:54 +0100

    I3: number guessing game complete

--- web_guesser/web_guesser.rb
+++ web_guesser/web_guesser.rb
@@ -1,6 +1,12 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+number = rand(100)
+
 get '/' do
-	erb :index , :locals => {:number => rand(100)}
+	erb :index , :locals => {:number => number}
 end
+
+# if params["guess"] > number
+# 	"#{params["guess"]} is higher than #{number}"
+# end

6681c208262f4e9976dd014dd12e80c01182959a
Salomanuel <salomanuel.aux+code@gmail.com>
2017-03-04 13:30:45 +0100

    add secret methods

--- Singing-with-Sinatra/basics.rb
+++ Singing-with-Sinatra/basics.rb
@@ -12,3 +12,23 @@ end
 get '/hello/:name' do
 	"ciao #{params[:name]}"
 end
+
+get '/form' do
+	erb :form
+end
+
+post '/form' do
+	"you said #{params[:message]}"
+end
+
+get '/secret' do
+	erb :secret
+end
+
+post '/secret' do
+	params[:secret].reverse
+end
+
+get '/notsecret/:secret' do
+	params[:secret].reverse
+end

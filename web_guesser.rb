3ebe8e563abeae52e72c3b5215ba89f9c3582e3a
Salomanuel <salomanuel.aux+code@gmail.com>
2018-02-03 17:32:13 +0100

    add slightly too small and slightly too big

--- web_guesser.rb
+++ web_guesser.rb
@@ -11,12 +11,15 @@ get '/' do
 	
 
 	case params['guess'].to_i
-	when 0...@@rando   then @@answer = "too small"
-	when @@rando       then @@answer = "you got it!"
-	when @@rando..100  then @@answer = "too big"
+	when @@rando       then @@answer = "you got it! it was #{@@rando}"
+	when (@@rando - 5)..@@rando then @@answer = "just a bit too small"
+	when @@rando..(@@rando + 5) then @@answer = "just a bit too big"
+	when 0...@@rando   then @@answer = "way too small"
+	when @@rando..100  then @@answer = "way too big"
 	end
-
+	
 	erb :index
 
 	# throw params.inspect
 end
+

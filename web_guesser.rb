011941fd54a37db9e64f52308fef705ccc64a7b9
Salomanuel <salomanuel.aux+code@gmail.com>
2018-02-03 17:23:43 +0100

    guesser now reacts to the guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,7 +2,21 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 	@@rando = rand(100)
+		# @@answer ||= "boh"
+
+
+
 
 get '/' do
+	
+
+	case params['guess'].to_i
+	when 0...@@rando   then @@answer = "too small"
+	when @@rando       then @@answer = "you got it!"
+	when @@rando..100  then @@answer = "too big"
+	end
+
 	erb :index
+
+	# throw params.inspect
 end

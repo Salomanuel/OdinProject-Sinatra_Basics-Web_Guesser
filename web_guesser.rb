0c0869bebf9487b37b53c17a0d5bc6c24467497a
Salomanuel <salomanuel.aux+code@gmail.com>
2018-02-03 18:47:49 +0100

    guessing game done

--- web_guesser.rb
+++ web_guesser.rb
@@ -9,17 +9,23 @@ require 'sinatra/reloader'
 
 get '/' do
 	
+	message = check_guess(params['guess'].to_i)
 
-	case params['guess'].to_i
+
+	
+	
+	erb :index
+
+	# throw params.inspect
+end
+
+def check_guess(guess)
+	case guess
 	when @@rando       then @@answer = "you got it! it was #{@@rando}"
 	when (@@rando - 5)..@@rando then @@answer = "just a bit too small"
 	when @@rando..(@@rando + 5) then @@answer = "just a bit too big"
 	when 0...@@rando   then @@answer = "way too small"
 	when @@rando..100  then @@answer = "way too big"
 	end
-	
-	erb :index
-
-	# throw params.inspect
 end
 
